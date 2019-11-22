64040a7dcf69bd4e7d4272a69290deb5ccc2add3
Gerax Sotelo <gsotelo23@gmail.com>
2019-11-22 11:34:46 -0700

    Adding example  template and logic to button

--- app/Controllers/ListController.js
+++ app/Controllers/ListController.js
@@ -1,13 +1,31 @@
 import ListService from "../Services/ListService.js";
+import _store from "../store.js"
 
 //TODO Don't forget to render to the screen after every data change.
-function _drawLists() {}
+function _drawLists() {
+  let template = ''
+  _store.State.lists.forEach(list => template += list.template)
+  document.querySelector("#lists").innerHTML = template
+}
 
 //Public
 export default class ListController {
   constructor() {
     //NOTE: After the store loads, we can automatically call to draw the lists.
     _drawLists();
+  }
+
+  addList(event) {
+    debugger;
+    event.preventDefault()
+    let formData = event.target
+    let listData = {
+      name: formData.name.value
+    }
+
+    ListService.addList(listData)
+    _drawLists()
+    formData.reset()
   }
 
   //TODO: Your app will need the ability to create, and delete both lists and listItems
--- app/Models/List.js
+++ app/Models/List.js
@@ -4,7 +4,32 @@ export default class List {
   constructor(data) {
     //TODO Your constructor takes in a data object that should have the properties you need to create your list here is a freebie, it will set the id its provided, or if that is undefined it will create a new one (this is an alternative to object destructuring)
     this.id = data.id || generateId();
+    this.name = data.name
   }
   //Be sure to add the methods needed to create the view template for this model
   //For starting out, your tasks may be strings alone, but later you may wish to turn them into full objects, that will be up to you
+
+  get template() {
+    return `
+      <div class="col-3 bg-info rounded">
+        <form>
+            <div class="form-group text-center">
+                <label for="formGroupExampleInput">
+                    <h2>${this.name}</h2>
+                </label>
+                <div class="row">
+                    <div class="col text-left">
+                        <p>list item</p>
+                    </div>
+                    <div class="col text-right">
+                        <i class="fas fa-trash-alt delete-btn rounded"></i>
+                    </div>
+                </div>
+                <input type="text" class="form-control" id="addList" placeholder="Add Task...">
+                <button class="btn btn-primary w-50 mt-2">Add</button>
+            </div>
+        </form>
+      </div>
+    `
+  }
 }
